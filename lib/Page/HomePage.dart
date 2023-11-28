87c9ca0d219ab8e6ee79402d987ee929dc6dbd70
josh_herban <102740327+VODO0331@users.noreply.github.com>
2023-11-28 16:46:23 +0800

    bulid ui 2

--- lib/Page/HomePage.dart
+++ lib/Page/HomePage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'dart:developer' as dev show log;
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -11,57 +12,141 @@ class HomePage extends StatelessWidget {
       padding: const EdgeInsets.only(top: 100),
       child: Column(
         children: [
-          Flex(
-            direction: Axis.horizontal,
-            children: [
+          appBar(),
+          const Divider(
+            thickness: 2,
+            color: Color.fromARGB(123, 255, 255, 255),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            padding: EdgeInsets.all(8),
+            width: 360.w,
+            height: 180.h,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Color.fromARGB(160, 73, 57, 34),
+              border: Border.all(color: Colors.white70),
+            ),
+            child: Flex(direction: Axis.horizontal, children: [
               Expanded(
-                flex: 2,
-                child: GestureDetector(
-                  onTap: (){
-                    dev.log('open user page');
-                  },
-                  child: Stack(alignment: Alignment.centerLeft, children: [
-                    SvgPicture.asset(
-                      "images/homePage/userBackground.svg",
-                      height: 40.h,
+                flex: 3,
+                child: Column(
+                  children: [
+                    Text('podcaster'),
+                    SizedBox(
+                      height: 4.h,
                     ),
-                    Align(
-                      alignment: Alignment.center,
+                    CircleAvatar(
+                      radius: 55,
+                      backgroundImage:
+                          AssetImage('images/turnTable/record.png'),
                       child: CircleAvatar(
-                        backgroundColor: Colors.white,
-                        radius: 17.h,
-                        child: CircleAvatar(
-                          radius: 16.h,
-                          child: Image.asset("images/drawer/people1.png"),
+                        radius: 40,
+                        child: Image.asset(
+                          'images/drawer/people2.png',
+                          fit: BoxFit.cover,
                         ),
                       ),
                     )
-                  ]),
+                  ],
                 ),
               ),
+              const VerticalDivider(
+                thickness: 1,
+                color: Color.fromARGB(255, 237, 186, 145),
+              ),
               Expanded(
-                flex: 7,
-                child: ElevatedButton.icon(
-                  style: ButtonStyle(
-                      iconColor: MaterialStateProperty.all(Colors.white),
-                      alignment: Alignment.centerLeft,
-                      backgroundColor:
-                          MaterialStateProperty.all(Colors.black45),
-                      shape: MaterialStateProperty.all(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20), 
-                        ),
-                      ),
-                      ),
-                  icon:const Icon(Icons.search),
-                  label:const Text(""),
-                  onPressed: () { dev.log('sarch');},
-                ),
-              )
-            ],
+                  flex: 6,
+                  child: Container(
+                      width: 200.w,
+                      height: 165.h,
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(96, 76, 74, 74),
+                          border: Border.all(color: Colors.white60),
+                          borderRadius: BorderRadius.circular(10)),
+                      child: ListView(
+                        children: const [
+                          Text('tile1'),
+                          Divider(
+                            thickness: 1,
+                            color: Colors.white,
+                          ),
+                          Text('tile2'),
+                          Divider(
+                            thickness: 1,
+                            color: Colors.white,
+                          ),
+                          Text('tile3'),
+                          Divider(
+                            thickness: 1,
+                            color: Colors.white,
+                          ),
+                        ],
+                      )))
+            ]),
           ),
         ],
       ),
     );
   }
+}
+
+Widget appBar() {
+  return Flex(
+    direction: Axis.horizontal,
+    children: [
+      Expanded(
+        flex: 2,
+        child: GestureDetector(
+          onTap: () {
+            dev.log('open user page');
+          },
+          child: Stack(alignment: Alignment.centerLeft, children: [
+            SvgPicture.asset(
+              "images/homePage/userBackground.svg",
+              height: 40.h,
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 17.h,
+                child: CircleAvatar(
+                  radius: 16.h,
+                  child: Image.asset("images/drawer/people1.png"),
+                ),
+              ),
+            )
+          ]),
+        ),
+      ),
+      Expanded(
+        flex: 7,
+        child: Padding(
+          padding: const EdgeInsets.only(right: 8.0),
+          child: ElevatedButton.icon(
+            style: ButtonStyle(
+              alignment: Alignment.centerLeft,
+              backgroundColor: MaterialStateProperty.all(Colors.black45),
+              shape: MaterialStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+            icon: const Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            label: const Text(""),
+            onPressed: () {
+              dev.log('sarch');
+            },
+          ),
+        ),
+      )
+    ],
+  );
 }
--- lib/Page/backgound.dart
+++ lib/Page/backgound.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class MyBackGround extends StatelessWidget {
   final Widget myWiget;
 
@@ -11,24 +10,24 @@ class MyBackGround extends StatelessWidget {
     return Container(
       decoration: const BoxDecoration(
         gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-          Color.fromARGB(255, 207, 140, 84),
-          Color(0xFF2C271D),
-        ],
-        stops: [0.1,1]
-        ),
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromARGB(255, 237, 186, 145),
+              Color(0xFF2C271D),
+            ],
+            stops: [
+              0.1,
+              1
+            ]),
       ),
       child: Stack(
         children: [
           Align(
-            alignment: Alignment.topLeft,
+            alignment: Alignment.topCenter,
             child: Image.asset("images/background/backGroundVine.png"),
           ),
-          
-           myWiget
-          
+          myWiget
         ],
       ),
     );
