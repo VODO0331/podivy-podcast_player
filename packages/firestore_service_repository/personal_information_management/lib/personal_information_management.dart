aa018fedc06887311109a2434ddd127a348fecb6
josh_herban <102740327+VODO0331@users.noreply.github.com>
2024-02-19 14:09:16 +0800

    檔案名小更改

--- packages/firestore_service_repository/personal_information_management/lib/personal_information_management.dart
+++ packages/firestore_service_repository/personal_information_management/lib/personal_information_management.dart
@@ -3,5 +3,5 @@
 library personal_information_management;
 
 export './src/models/user_info.dart';
-export './src/service/personal_information_management.dart';
+export './src/service/personal_information_managementFormGetx.dart';
 // export './src/service/personal_information_management2.dart';
